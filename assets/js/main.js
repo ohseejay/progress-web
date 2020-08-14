4332f6a7d264e5da16b3180f6f994336f76dc0fe
Jana <jbpavlasek@gmail.com>
2020-08-13 23:13:36 -0400

    Add slideshow control

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,47 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-function carousel() {
+var slideIndex = 1;
+var timeout;
+
+// Next/previous controls
+function plusSlides(n) {
+  clearTimeout(timeout);
+  slideIndex += n;
+  carousel(slideIndex);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	clearTimeout(timeout);
+	slideIndex = n;
+  carousel(slideIndex);
+}
+
+function carousel(n) {
   var i;
   var x = document.getElementsByClassName("slideshow");
-  if (x.length === 0)
-  {
+  var dots = document.getElementsByClassName("dot");
+  if (x.length === 0) {
   	return;
   }
+
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slideIndex = n;
+  if (slideIndex > x.length) { slideIndex = 1; }
+  if (slideIndex < 1) { slideIndex = x.length; }
+
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+  dots[slideIndex-1].className += " active";
+
+  timeout = setTimeout(function(){ carousel(slideIndex + 1) }, 5000); // Change image every 5 seconds
 }
 
 
@@ -78,7 +105,6 @@ function carousel() {
 					visibleClass: 'navPanel-visible'
 				});
 
-	slideIndex = 0;
-	carousel();
+	carousel(slideIndex);
 
 })(jQuery);
